2732d621245e529a06df3d1cb63c3f43b1d6f698
Eduard Kazachkov <RussianEHOT5@yandex.ru>
2023-12-01 23:37:42 +0300

    4

---
+++ backend/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import 
+
+router = routers.DefaultRouter()
+
+router.register(r'recipes', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
