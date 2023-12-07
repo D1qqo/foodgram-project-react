660f26e8096c1a5378bc2dde945f39abc9e5721e
Eduard Kazachkov <RussianEHOT5@yandex.ru>
2023-12-07 03:09:36 +0300

    18

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -26,8 +26,8 @@ class UsersCreateSerializer(UserCreateSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name',
-                  'last_name', 'email', 'password')
+        fields = ('id', 'email', 'username',
+                  'first_name', 'last_name', 'password')
 
 
 class IngredientSerializer(serializers.ModelSerializer):
