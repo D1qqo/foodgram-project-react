5219a4ff739da539d831e23b09559b5ddb1096ba
root <root@epdgpgsmirhsnvompi4k.auto.internal>
2023-12-21 00:59:58 +0000

    3.19

---
+++ backend/recipes/migrations/0001_initial.py
@@ -0,0 +1,91 @@
+import colorfield.fields
+import django.core.validators
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Favorite',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            ],
+            options={
+                'verbose_name': 'Избранное',
+                'verbose_name_plural': 'Избранные',
+            },
+        ),
+        migrations.CreateModel(
+            name='Ingredient',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Название')),
+                ('measurement_unit', models.CharField(max_length=256, verbose_name='Единица измерения')),
+            ],
+            options={
+                'verbose_name': 'Ингредиент',
+                'verbose_name_plural': 'Ингредиенты',
+                'ordering': ('name',),
+            },
+        ),
+        migrations.CreateModel(
+            name='IngredientsInRecipe',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('amount', models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(limit_value=0.1, message='Количество ингредиентов не может быть меньше 0.1')], verbose_name='Количество')),
+            ],
+            options={
+                'verbose_name': 'Ингредиент в рецепте',
+                'verbose_name_plural': 'Ингредиенты в рецепте',
+                'ordering': ('recipe__name',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Recipe',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Название')),
+                ('image', models.ImageField(null=True, upload_to='recipes/image', verbose_name='Картинка')),
+                ('text', models.TextField(verbose_name='Описание')),
+                ('cooking_time', models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(limit_value=1, message='Время приготовления не может быть меньше 1 минуты')], verbose_name='Время готовки')),
+            ],
+            options={
+                'verbose_name': 'Рецепт',
+                'verbose_name_plural': 'Рецепты',
+                'ordering': ('-id',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=256, unique=True, verbose_name='Название')),
+                ('color', colorfield.fields.ColorField(default='#FF0000', image_field=None, max_length=7, samples=None, unique=True, verbose_name='Цвет')),
+                ('slug', models.SlugField(unique=True, verbose_name='Слаг')),
+            ],
+            options={
+                'verbose_name': 'Тег',
+                'verbose_name_plural': 'Теги',
+                'unique_together': {('name', 'slug')},
+            },
+        ),
+        migrations.CreateModel(
+            name='ShoppingCart',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('recipe', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='shopping_cart', to='recipes.recipe', verbose_name='Рецепт')),
+            ],
+            options={
+                'verbose_name': 'Список покупок',
+                'verbose_name_plural': 'Списки покупок',
+                'default_related_name': 'shopping_cart',
+            },
+        ),
+    ]
